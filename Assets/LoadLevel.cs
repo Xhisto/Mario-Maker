b49d48bb5106e038d03c5f3f7a629f771ef51f2a
CripC <Alrik-Mark.Brillantes@elev.ga.lbs.se>
2018-11-13 10:51:40 +0100

    First time
    Good Game

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+    public string SceneToLoad = "First";
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movespeed = 7f;
+    public float jumpspeed = 15f;
+
+    private Rigidbody2D rbody;
+    public GroundChecker Groundcheck;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * movespeed, rbody.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Groundcheck.isGrounded == true)
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpspeed);
+
+        }
+    }
+}

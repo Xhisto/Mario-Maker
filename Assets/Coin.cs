0266bc076f8de03f5d3d3cebea795be349d7026a
CripC <Alrik-Mark.Brillantes@elev.ga.lbs.se>
2018-11-23 15:00:26 +0100

    V.1
    PLatformer

---
+++ Assets/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int amount = 1;
+
+    public float spinspeed = 180;
+
+    private void Update()
+    {
+        transform.Rotate(0, spinspeed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/EnemyRatTing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRatTing : MonoBehaviour
+{
+    public float Speed = 10f;
+    private Rigidbody2D rbody;
+    public bool left = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (left == true)
+        {
+            rbody.velocity = -(Vector2)transform.right * Speed;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rbody.velocity = (Vector2)transform.right * Speed;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "wall")
+        {
+        left = !left;
+        }
+    }
+}
